dc7f5364e40970e0d46c437a978133b7a4a9b1ba
Numeracy0659 <alinumeracy@gmail.com>
2026-09-10 02:11:48 +0100

    Add ProjectSettingsActivity class

---
+++ app/src/main/java/com/numeracy/termuxide/ui/activities/ProjectSettingsActivity.java
@@ -0,0 +1,15 @@
+package com.numeracy.termuxide.ui.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.numeracy.termuxide.R;
+
+public class ProjectSettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_project_settings);
+    }
+}
